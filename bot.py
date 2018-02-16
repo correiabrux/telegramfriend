5df557ac9f45f153ef861a7aa6653e2af8679062
correiabrux <bruno.correia@luizalabs.com>
2018-02-16 12:18:18 -0200

    News All Terms

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ from newsapi import NewsApiClient
 def listen(msg):
     *_, chat_id = telepot.glance(msg)
     content = msg['text']
+    content = content.lower()
     proccess(chat_id, content)
 
 def proccess(chat_id, content):
@@ -16,18 +17,19 @@ def proccess(chat_id, content):
         content = "Hello, how are you?"
     elif content == "/start":
         content = "Hello, how are you?"
-    elif content == "news":
+    elif content.startswith('news'):
+        terms = content.split(" ")[1]
         content = "News Delivered :)"
-        news(chat_id)
+        news(chat_id, terms)
     else:
         content = "Sorry, I do not understand."
     sendMsg(chat_id, content)   
 
-def news(chat_id):
+def news(chat_id, terms='news'):
     now = datetime.datetime.now()
     date = now.strftime("%Y-%m-%d")
     newsapi = NewsApiClient(api_key=(os.environ['newsapikey']))
-    content = newsapi.get_everything(q='news'
+    content = newsapi.get_everything(q=terms
                                         ,sources='bbc-news,the-verge'
                                         ,domains='bbc.co.uk,techcrunch.com'
                                         ,from_parameter=date
